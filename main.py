c009d9c95a8299ce43133452037aea84d5b6991b
Abdel-Rahman Abdel-Rahman <abogical@gmail.com>
2020-04-28 14:44:18 +0200

    Recieve and act upon broadcast messages

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import socket
 import time
 import uuid
 import struct
+from datetime import datetime, timezone
+import re
 # https://bluesock.org/~willkg/dev/ansi.html
 ANSI_RESET = "\u001B[0m"
 ANSI_RED = "\u001B[31m"
@@ -40,10 +42,10 @@ def get_node_uuid():
 
 
 class NeighborInfo(object):
-    def __init__(self, delay, last_timestamp, ip=None, tcp_port=None):
+    def __init__(self, delay, last_timestamp, broadcast_count, ip=None, tcp_port=None):
         # Ip and port are optional, if you want to store them.
         self.delay = delay
-        self.last_timestamp = last_timestamp
+        self.broadcast_count = broadcast_count
         self.ip = ip
         self.tcp_port = tcp_port
 
@@ -66,6 +68,9 @@ broadcaster = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Setup the UDP socket
 broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 broadcaster.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+broadcaster.bind(('', get_broadcast_port()))
+
+broadcast_msg_re = re.compile(r'([\da-f]{8}) ON (\d{1,5})')
 
 
 def send_broadcast_thread():
@@ -83,9 +88,20 @@ def receive_broadcast_thread():
     and exchange timestamps.
     """
     while True:
-        # TODO: write logic for receiving broadcasts.
         data, (ip, port) = broadcaster.recvfrom(4096)
         print_blue(f"RECV: {data} FROM: {ip}:{port}")
+        broadcast_match = broadcast_msg_re.match(data.decode('ascii'))
+        print(broadcast_match, data.decode('ascii'))
+        if broadcast_match:
+            uuid = broadcast_match.group(1)
+            if uuid not in neighbor_information or neighbor_information[uuid].broadcast_count == 9:
+                daemon_thread_builder(exchange_timestamps_thread, (uuid, ip, int(broadcast_match.group(2)))).start()
+            else:
+                neighbor_information[uuid].broadcast_count += 1
+
+
+def timestamp():
+    return round(datetime.now(timezone.utc).timestamp())
 
 
 def tcp_server_thread():
@@ -103,8 +119,13 @@ def exchange_timestamps_thread(other_uuid: str, other_ip: str, other_tcp_port: i
 
     Then update the neighbor_info map using other node's UUID.
     """
+    my_timestamp = timestamp()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print_yellow(f"ATTEMPTING TO CONNECT TO {other_uuid}")
-    pass
+    client.connect((other_ip, other_tcp_port))
+    client.send(hex(my_timestamp)[2:])
+    other_timestamp = int(server.recvfrom(1024)[0], base=16)
+    neighbor_information[other_uuid] = NeighborInfo(other_timestamp-my_timestamp, other_timestamp, 0 if other_uuid in neighbor_information else 1, other_ip, other_tcp_port)
 
 
 def daemon_thread_builder(target, args=()) -> threading.Thread:
@@ -117,9 +138,14 @@ def daemon_thread_builder(target, args=()) -> threading.Thread:
 
 
 def entrypoint():
-    broadcast_thread = daemon_thread_builder(send_broadcast_thread)
-    broadcast_thread.start()
-    broadcast_thread.join()
+    daemon_thread_builder(receive_broadcast_thread).start()
+    daemon_thread_builder(send_broadcast_thread).start()
+    while True:
+        # Delete any lost nodes
+        time.sleep(1)
+        min_timestamp = timestamp() - 10
+        for key in filter(lambda key: neighbor_information[key].last_timestamp < min_timestamp, neighbor_information.keys()):
+            del neighbor_information[key]
 
 ############################################
 ############################################
